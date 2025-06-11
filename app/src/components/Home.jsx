fdeeaa3e3eaec7a24f8df82c07bc04644e90f1aa
NellitG <kamaunelson157@gmail.com>
2025-06-11 17:07:56 +0300

    Change package dependencies

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="p-6 text-center text-2xl font-bold text-blue-600">
+    <div className="p-6 text-center text-2xl font-bold bg-yellow-100 text-blue-600">
       Welcome to the Home Page
     </div>
   );
